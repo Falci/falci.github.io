2321589065437a5bba54d3e201776fd6df503f15
Fernando Falci <falci@falci.me>
2016-01-02 20:36:47 -0200

    Fix form

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,61 +1,67 @@
 $(function() {
 
- $("input,textarea").jqBootstrapValidation(
-    {
-     preventSubmit: true,
-     submitError: function($form, event, errors) {
-      // something to have when submit produces an error ?
-     },
-     submitSuccess: function($form, event) {
-      event.preventDefault(); // prevent default submit behaviour
-       // get values from FORM
-       var name = $("input#name").val();
-       var email = $("input#email").val();
-       var message = $("textarea#message").val();
-        var firstName = name; // For Success/Failure Message
-           // Check for white space in name for Success/Fail message
+  $("input,textarea").jqBootstrapValidation({
+    preventSubmit: true,
+    submitSuccess: function($form, event) {
+      event.preventDefault();
+
+      var name = $("input#name").val(),
+        email = $("input#email").val(),
+        message = $("textarea#message").val(),
+        firstName = name;
+
         if (firstName.indexOf(' ') >= 0) {
-     firstName = name.split(' ').slice(0, -1).join(' ');
-         }
-   $.ajax({
-              url: "//formspree.io/contato@falci.me",
-              type: "POST",
-              data: {name: name, email: email, message: message},
-              cache: false,
-              success: function() {
-              // Success message
-                 $('#success').html("<div class='alert alert-success'>");
-                 $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                .append( "</button>");
-                $('#success > .alert-success')
-                .append("<strong>Your message has been sent. </strong>");
-       $('#success > .alert-success')
-       .append('</div>');
+          firstName = name.split(' ').slice(0, -1).join(' ');
+        }
 
-       //clear all fields
-       $('#contactForm').trigger("reset");
-         },
-      error: function() {
-     // Fail message
-      $('#success').html("<div class='alert alert-danger'>");
-              $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-               .append( "</button>");
-              $('#success > .alert-danger').append("<strong>Sorry "+firstName+" it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:me@example.com?Subject=Message_Me from myprogrammingblog.com'>me@example.com</a> ? Sorry for the inconvenience!");
-           $('#success > .alert-danger').append('</div>');
-     //clear all fields
-     $('#contactForm').trigger("reset");
-       },
-           })
-         },
-         filter: function() {
-                   return $(this).is(":visible");
-         },
-       });
+        $.ajax({
+          url: "//formspree.io/contato@falci.me",
+          type: "POST",
+          data: {name: name, email: email, message: message},
+          cache: false,
+          success: function() {
+            $('#success')
+              .html("<div class='alert alert-success'>");
 
-      $("a[data-toggle=\"tab\"]").click(function(e) {
-                    e.preventDefault();
-                    $(this).tab("show");
-        });
+            $('#success > .alert-success')
+              .html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+              .append( "</button>");
+
+            $('#success > .alert-success')
+              .append("<strong>Sua mensagem foi enviada. </strong>");
+
+            $('#success > .alert-success')
+              .append('</div>');
+
+            $('#contactForm').trigger("reset");
+          },
+          error: function() {
+
+            $('#success')
+              .html("<div class='alert alert-danger'>");
+
+            $('#success > .alert-danger')
+              .html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+              .append( "</button>");
+
+            $('#success > .alert-danger')
+              .append("<strong>Desculpe "+firstName+", parece que algo está errado com meu servidor de email...</strong> Você pode enviar um email direto para <a href='mailto:contato@falci.me'>contato@falci.me</a> ? Desculpe pela inconveniencia!");
+
+            $('#success > .alert-danger').append('</div>');
+
+            $('#contactForm').trigger("reset");
+          },
+        })
+      },
+      filter: function() {
+        return $(this).is(":visible");
+      },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+      e.preventDefault();
+      $(this).tab("show");
+    });
   });
 
 
